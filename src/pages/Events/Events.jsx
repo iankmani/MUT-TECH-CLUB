a583bff60b29cdcd94f5ada7541af6236496d311
Ian Kimani <ian.kiarie2002@gmail.com>
2024-06-02 23:38:55 +0300

    add events page

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -3,7 +3,42 @@ const Events = () => {
     return(
         <div className="events">
             <h1>Events</h1>
-            
+            <div>
+            <div className="container">
+        <ul className="event-list">
+            <li className="event">
+                <h2><span className="day">Monday:</span> cyber Security Led By Webster Ifedha</h2>
+                <p>Time: 4:00 PM - 7:00 PM</p>
+                <p>Description: Computer Lab F04.</p>
+            </li>
+            <li className="event">
+                <h2><span className="day">Tuesday:</span> UI/UX Led By Manase Gunga</h2>
+                <p>Time: 5:00 PM - 8:00 PM</p>
+                <p>Description: Computer Lab F04.</p>
+            </li>
+            <li className="event">
+                <h2><span className="day">Wednesday:</span> Web Development By Caroline Githenduka</h2>
+                <p>Time: 6:00 PM - 9:00 PM</p>
+                <p>Description: Computer Lab F04.</p>
+            </li>
+            <li className="event">
+                <h2><span className="day">Thursday:</span> Mobile Apps Development By Stanley Amunze</h2>
+                <p>Time: 4:00 PM - 7:00 PM</p>
+                <p>Description: Computer Lab F04.</p>
+            </li>
+            <li className="event">
+                <h2><span className="day">Friday:</span> Cloud Engineering By Paul karanja</h2>
+                <p>Time: 3:00 PM - 6:00 PM</p>
+                <p>Description: Computer Lab F04.</p>
+            </li>
+            <li className="event">
+                <h2><span className="day">Saturday:</span> Power Platform By Evyonn Mbithe</h2>
+                <p>Time: 1:00 PM - 4:00 PM</p>
+                <p>Description: Computer Lab F04.</p>
+            </li>
+        </ul>
+    </div>
+            </div>
         </div>
         
 
